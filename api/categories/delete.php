d1c667891cbfac860bd4bda3ed66513a3a116dfc
Cierra Ray <cierraray2@gmail.com>
2024-03-04 22:21:11 -0500

    changes successful before this

--- api/categories/delete.php
+++ api/categories/delete.php
@@ -1,14 +1,15 @@
 <?php 
   // Headers
   header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    $method = $_SERVER['REQUEST_METHOD'];
+  header('Content-Type: application/json');
+  $method = $_SERVER['REQUEST_METHOD'];
 
-    if ($method === 'OPTIONS') {
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-        exit();
-    }
+  if ($method === 'OPTIONS') {
+      header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+      header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+      exit();
+  }
+
   include_once '../../config/Database.php';
   include_once '../../models/Category.php';
 
