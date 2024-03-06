c126601461e6e8202fc8f0fbd0aa8062b6331264
Cierra Ray <cierraray2@gmail.com>
2024-03-05 19:16:04 -0500

    trying to get quotes working and 45 tests passed

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -32,7 +32,10 @@
   // Create quote
   if($quote->create()) {
     echo json_encode(
-      array('id'=>$quote->create())
+      array('id'=>$quote->create(),
+      'quote'=>$quote->quote,
+      'author_id'=>$quote->author_id,
+      'category_id'=> $quote->category_id)
     );
   } else {
     echo json_encode(
