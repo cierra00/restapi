55334ddf212c76af4113e68ef28f0dd2bdc87754
Cierra Ray <cierraray2@gmail.com>
2024-03-06 20:04:54 -0500

    Testing author_id not found

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -29,11 +29,21 @@
   $quote->quote = $data->quote;
   $quote->author_id = $data->author_id;
   $quote->category_id = $data->category_id;
+  
+  $post_arr = array(
+    'id'=> $quotes->id,
+    'quote'=> $quotes->quote,
+    #'author_id'=> $quotes->author_id,
+    'category'=> $quotes->category_name,
+    #'category_id'=> $quotes->category_id,
+    'author' => $quotes->author_name   
+
+);
 
   // Create quote
   if($quote->update()) {
     echo json_encode(
-      array('message' => 'Quote updated')
+      $post_arr
     );
   } else {
     echo json_encode(
