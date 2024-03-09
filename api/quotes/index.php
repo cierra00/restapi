0034fcfa23e81a0e3eaa8f3dd42a69b942163bbb
Cierra Ray <cierraray2@gmail.com>
2024-03-09 16:15:39 -0500

    now passing 56 working on author_id not found

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -51,11 +51,6 @@ if($method=== 'POST' || $method==='PUT'){
         echo json_encode(array('message'=> 'author_id Not Found'));
     }
 }
-if($method=== 'POST' || $method==='PUT'){
-    if($data->category_id !== $quote->category_id){
-        echo json_encode(array('message'=> 'category_id Not Found'));
-    }
-}
 
 
   if ($method === 'GET' && !$id) { require_once('read.php'); };
