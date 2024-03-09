4aa3d27b7abed58474dfdc32ce7ca93147fd94de
Cierra Ray <cierraray2@gmail.com>
2024-03-09 13:18:42 -0500

    fixing GET/CAT

--- api/categories/read_single.php
+++ api/categories/read_single.php
@@ -1,24 +1,19 @@
 <?php
 
-
-
-
-
-
 #get ID from URL
-$author->id = isset($_GET['id']) ? $_GET['id'] : die();
+$category->id = isset($_GET['id']) ? $_GET['id'] : die();
 
 #Get Single Author 
-$author->read_single();
+$category->read_single();
 
 #create array
 $post_arr = array(
-    'id'=> $author->id,
-    'author'=> $author->name,
+    'id'=> $category->id,
+    'category'=> $category->name,
     
 );
 
-if($author->id){
+if($category->id){
     print_r(json_encode($post_arr));
     } else {
         echo json_encode(
