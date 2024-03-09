6399e28d3efd7ba29371f50e27f545dfaad496a9
Cierra Ray <cierraray2@gmail.com>
2024-03-09 12:09:37 -0500

    reverting Quotes index and testing

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -11,6 +11,9 @@
 
     require_once('../../config/Database.php');
     require_once('../../models/Quote.php');
+    require_once('../../models/Author.php');
+    require_once('../../models/Category.php');
+    require_once('../../functions/isValid.php');
    
 
 
