a5886bb7afbed23638025f9da0bd15aca50b337b
Cierra Ray <cierraray2@gmail.com>
2024-03-09 12:05:24 -0500

    reverted quotes delete

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -11,15 +11,11 @@
 
     require_once('../../config/Database.php');
     require_once('../../models/Quote.php');
-    require_once('../../models/Category.php');
-    require_once('../../models/Author.php');
-    require_once('../../functions/isValid.php');
    
 
 
      /* Initialization of variables*/
-      $id = null;
-     
+     $id = null;
 
      
 
@@ -30,26 +26,11 @@
 
     /* Category Object */
 
-    $quote = new Quote($db);
-    $author = new Author($db);
-    $category = new Category($db);
-
+    $category = new Quote($db);
 
     // Handle ID from parameter in URL
     if ($method === 'GET') {
         $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
-       
-    }
-
-     // DELETE and UPDATE/PUT need to validate the (quote) id parameter
-     if ($method === 'DELETE' || $method === 'PUT') {
-        $idExists = isValid($id, $quote);
-        if (!$idExists) { 
-            echo json_encode(
-                array('message' => 'No Quotes Found')
-            );
-            
-        }
     }
    
 
