99374069af593ca1e6c1bcd1d9f09e458631b272
Cierra Ray <cierraray2@gmail.com>
2024-03-09 18:30:49 -0500

    57 tests passed. Testing category_id/author_id in quotes

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -4,12 +4,13 @@
   //set id to delete
   $quote->id = $data->id;
   
-
-  //Delete quote
-  if($quote->delete()) {
-    echo json_encode($quote->delete());
-  } else {
-    echo json_encode(
-      array('message' => 'No Quotes Found')
-    );
-  }
+try{
+  $quote->delete();
+  echo json_encode($quote->delete());
+}
+catch(exception $e){
+  echo json_encode(
+    array('message' => 'No Quotes Found')
+  );
+}
+ 
