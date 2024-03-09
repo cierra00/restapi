aab5c2bf23723d7e512c6f0470cde294a9797b80
Cierra Ray <cierraray2@gmail.com>
2024-03-09 14:37:47 -0500

    now passing 54 tests. Currently working on No Quotes found message on quotes

--- api/authors/update.php
+++ api/authors/update.php
@@ -1,6 +1,28 @@
 <?php 
- 
-  
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  $method = $_SERVER['REQUEST_METHOD'];
+
+  if ($method === 'OPTIONS') {
+      header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+      header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+      exit();
+  }
+
+  include_once '../../config/Database.php';
+  include_once '../../models/Author.php';
+
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate blog author object
+  $author = new Author($db);
+
+  // Get raw author data
+  $data = json_decode(file_get_contents("php://input"));
+
   //set id to update
 
   $author->id = $data->id;
@@ -8,17 +30,14 @@
   
 
   // Create author from db
-  if (
-    !empty($data->name)  && !empty($id)
-    ) { try{
-      $output = $author->update();
-      echo json_encode($output);
-    } catch(Exception $e){
-      echo json_encode(
-        array('message' => $e->getMessage())
+  if($author->update()) {
+    echo json_encode(
+      array('id' => $data->id,
+      'author' => $data->name
+      )
     );
-    }}  else {
-      echo json_encode(
-          array('message' => 'Missing Required Parameters')
-      );
+  } else {
+    echo json_encode(
+      array('message' => 'Missing Required Parameters')
+    );
   }
